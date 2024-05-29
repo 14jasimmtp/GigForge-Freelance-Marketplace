3e873f035f47c52ca384055925e3d1d3d280cc90
Muhamed Jasim <jasimmtp84@gmail.com>
2024-05-29 18:58:07 +0530

    updated api-gateway

--- API-Gateway/pkg/client/authClient.go
+++ API-Gateway/pkg/client/authClient.go
@@ -1,18 +1,19 @@
 package client
 
 import (
-	"log"
 	"fmt"
+	"log"
 
 	"github.com/14jasimmtp/GigForge-Freelancer-Marketplace/pb/auth"
 	"github.com/14jasimmtp/GigForge-Freelancer-Marketplace/pkg/config"
+	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 func InitAuthClient(c *config.Config) auth.AuthServiceClient {
 	fmt.Println("workig new image ")
-	conn, err := grpc.Dial(c.AUTH_SVC, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(viper.GetString("AUTH_SVC"), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatal("error connecting auth svc : ", err)
 	}
